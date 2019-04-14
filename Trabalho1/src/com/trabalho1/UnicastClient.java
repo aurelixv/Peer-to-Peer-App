c1c8a7be0a5ef8706aa3c6bb52b6417eb50dda0a
Aurelio <aurelio@alunos.utfpr.edu.br>
2019-04-14 17:20:13 -0300

    yet more minor fixes

--- Trabalho1/src/com/trabalho1/UnicastClient.java
+++ Trabalho1/src/com/trabalho1/UnicastClient.java
@@ -12,18 +12,21 @@ public class UnicastClient extends Thread {
     public UnicastClient(int port) {
         this.hostName = "127.0.0.1";
         this.port = port;
-        try {
-            System.out.println("\nEscravo " + PeerInfo.name + " se conectando com o mestre na porta " + port + "...\n");
-            client = new Socket(hostName, port);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     public void run() {
         try {
-            System.out.println();
+            System.out.println("\nEscravo " + PeerInfo.name + " se conectando com o mestre na porta " + port + "...\n");
             client = new Socket(hostName, port);
+
+            while(true) {
+                // Comunica com o mestre
+                try {
+                    Thread.sleep(100000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
